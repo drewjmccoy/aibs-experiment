fa006ac7d62b714e06fefe841eebf6460ac38238
Drew McCoy <drewm@alleninstitute.org>
2016-06-15 15:38:51 -0700

    fixed length issue

--- SkeletonTest.py
+++ SkeletonTest.py
@@ -8,20 +8,19 @@ class Skeleton:
 
     def __init__(self, window, vertices):
         self.vertices = vertices
-        self.length = len(vertices)
         self.skeleton = visual.ShapeStim(win=window,
                                         vertices=self.vertices,
                                         closeShape=False
                                         )
-        self.shapeList = []
+        self._shapeList = []
         self._createShapeList(window=window)
 
     def draw(self):
-        for i in range(len(self.shapeList)):
-            self.shapeList[i].draw()
+        for i in range(len(self._shapeList)):
+            self._shapeList[i].draw()
 
     def _createShapeList(self, window):
-        if self.length > 0:
+        if len(self.vertices) > 0:
             # create first circle and add (fence post)
             circle = visual.Circle(win=window,
                                     radius = 0.5,
@@ -29,11 +28,11 @@ class Skeleton:
                                     fillColor = "white",
                                     pos = self.vertices[0]
                                     )
-            self.shapeList.append(circle)
+            self._shapeList.append(circle)
 
             # add rest of the 'fence'
             vertex = 1
-            while vertex < self.length:
+            while vertex < len(self.vertices):
                 # draw rectangle
 
                 # create end circle
@@ -43,5 +42,5 @@ class Skeleton:
                                         fillColor = "white",
                                         pos = self.vertices[vertex]
                                         )
-                self.shapeList.append(circleEnd)
+                self._shapeList.append(circleEnd)
                 vertex += 1
