beea327b7e45eb530f4768e3c2cecc4d5a05df02
Drew McCoy <drewm@alleninstitute.org>
2016-06-16 11:03:58 -0700

    added _getOrienation() and _getPosition() to Rectangle

--- shapes.py
+++ shapes.py
@@ -1,28 +1,60 @@
 # @author Drew McCoy <drewm@alleninstitute.org>
-# defines Skeleton
+# defines Rectangle, Skeleton
 
 from psychopy import visual
 import math
 
+# class that defines a Rectangle ShapeStim, given two end points
 class Rectangle:
 
-    def __init__(self, window, x, y):
+    def __init__(self, window, p0, p1):
         self._rectangle = visual.Rect(win=window,
                                       fillColor="white",
-                                      width=self._getWidth(x, y),
+                                      width=self._getWidth(p0, p1),
                                       height=self._getHeight(),
-                                      pos=(0,1),
-                                      ori=self._getOrientation(x, y)
+                                      pos=self._getPosition(p0, p1),
+                                      ori=self._getOrientation(p0, p1)
                                       )
     def _getHeight(self):
         return 1
 
-    def _getWidth(self, x, y):
-        return math.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2)
+    def _getWidth(self, p0, p1):
+        return math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)
 
-    def _getOrientation(self, x, y):
-        # TODO
-        return 0
+    def _getPosition(self, p0, p1):
+        # use midpoint formula
+        print "(" + str(p0) + ", " + str(p1) + ")"
+        x = (p1[0] + p0[0]) / 2
+        y = (p1[1] + p0[1]) / 2
+        print "(" + str(x) + ", " + str(y) + ")"
+        return (x, y)
+
+    def _getOrientation(self, p0, p1):
+        # get change y and x
+        dy = p1[1] - p0[1]
+        dx = p1[0] - p0[0]
+
+        # if slope is undefined
+        if dx is 0:
+            if dy < 0:
+                return 45
+            else:
+                return 270
+
+        # get slope
+        slope = dy / dx
+
+        # get temp radians of orientation (neg b/c psychopy is weird)
+        rad = -math.atan(slope)
+
+        # to degrees
+        deg = math.degrees(rad)
+
+        # if dy is neg, add 180
+        if dy < 0:
+            deg += 180
+
+        return deg
 
 # draws a ShapeStim from a skeleton (list of verices)
 class Skeleton:
@@ -55,12 +87,12 @@ class Skeleton:
             vertex = 1
             while vertex < len(self.vertices):
                 # draw rectangle
-                if vertex is 1:
-                    rectangle = Rectangle(window=window,
-                                          x=self.vertices[vertex - 1],
-                                          y=self.vertices[vertex]
-                                          )
-                    self._shapeList.append(rectangle._rectangle)
+                # if vertex is 1:
+                rectangle = Rectangle(window=window,
+                                      p0=self.vertices[vertex - 1],
+                                      p1=self.vertices[vertex]
+                                      )
+                self._shapeList.append(rectangle._rectangle)
 
                 # create end circle
                 circleEnd = visual.Circle(win=window,
