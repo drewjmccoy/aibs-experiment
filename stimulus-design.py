0833bdac45de9551bb89666e63b2937c63cece69
Drew McCoy <drewm@alleninstitute.org>
2016-06-17 15:25:54 -0700

    minor testing tweaks

--- stimulus-design.py
+++ stimulus-design.py
@@ -39,8 +39,14 @@ node4 = SkeletonNode(position=(0, 250))
 node3 = SkeletonNode(position=(-100, 150), connections=[node4])
 node2 = SkeletonNode(position=(100, 100), connections=[node3])
 node1 = SkeletonNode(position=(0, 0), connections=[node2, node4])
+node = SkeletonNode(position=(0, 0), connections=[node2])
 
-skel = SkeletonStim(window=mywin, root=node1, thickness=50)
+skel1 = SkeletonStim(window=mywin, root=node3)
+skel2 = SkeletonStim(window=mywin, root=node2)
+skel3 = SkeletonStim(window=mywin, root=node)
+skel4 = SkeletonStim(window=mywin, root=node1, thickness=10)
+
+skel = skel4
 
 # -------------------------------------MAIN LOOP-----------------------------------------
 
